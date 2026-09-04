6486286d783c7f2951c56a7bdca69c00ecd2ad3d
Jereson <2604311@sit.singaporetech.edu.sg>
2026-09-04 11:28:36 +0800

    Activity 4

--- hello.py
+++ hello.py
@@ -19,3 +19,13 @@ print ("Day 3:", followers)
 print ("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("===================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)

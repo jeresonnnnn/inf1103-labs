d996046e587b1bb460e35986b96060ba11691b39
Jereson <2604311@sit.singaporetech.edu.sg>
2026-09-18 11:52:56 +0800

    valid input function

--- Modular_auditor.py
+++ Modular_auditor.py
@@ -2,9 +2,21 @@ inventory = 0
 failed_entries = 0
 
 def get_valid_input():
-        user_input = input("Enter stock value (or type 'quit' to end system): ")
-        return user_input
+    user_input = input("Enter stock value (or type 'quit' to end system): ")
 
+    if user_input == "quit":
+        return "quit"
+
+    if user_input.isdigit():
+        value = int(user_input)
+        if value < 0:
+            print ("Error! Negative number is not a valid entry. Please try again!")
+            return None
+        return value
+    else:
+        print(f"Error! '{user_input}' is not a valid entry. Please try again!")
+        return None
+        
 while True :
     user_input = get_valid_input()
 

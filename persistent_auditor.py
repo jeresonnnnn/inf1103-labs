5db48f716c26c448f07c6ca63fec1f21351c8f78
Jereson <2604311@sit.singaporetech.edu.sg>
2026-09-25 15:04:45 +0800

    final save_inventory working

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -2,11 +2,11 @@ def count_items(list):
     count = 0
     for item in list:
         count = count + 1
-        return count
+    return count
 
 def load_inventory(filename="orders.txt"):
     orders = []
-    with open (filename, "a") as f:
+    with open(filename, "a") as f:
         pass
 
     with open(filename, "r") as f:
@@ -21,11 +21,10 @@ def load_inventory(filename="orders.txt"):
             if count_items(parts) == 3:
                 order_id = int(parts[0].strip())
                 product_name = parts[1].strip()
-                quantity = int(parts[3].strip())
+                quantity = int(parts[2].strip())
 
                 orders.append([order_id, product_name, quantity])
-
-                return orders
+    return orders
 
 def save_inventory(orders, filename="orders.txt"):
      with open(filename, "w") as f:
@@ -34,10 +33,31 @@ def save_inventory(orders, filename="orders.txt"):
                product_name = order[1]
                quantity = order[2]
                f.write(str(order_id) + "," + product_name + "," + str(quantity) + "\n")
-               
+
+def get_order_id(orders):
+    if count_items(orders) == 0:
+        return 1001
+
+    highest_id = orders[0][0]
+
+    for order in orders:
+        if order[0] > highest_id:
+            highest_id = order[0]
+    return highest_id + 1
+
+def display_order(orders):
+     print("\nCurrent Orders:\n")
+
+     for order in orders:
+          order_id = order[0]
+          product_name = order[1]
+          quantity = order[2]
+          print(str(order_id) + ", " + product_name + ", " + str(quantity))
 
 def main():
     orders = load_inventory()
+
+    display_order(orders)
 
     while True:
         user_input = input("\nEnter Product Name (or type 'quit' to end): ")
@@ -54,7 +74,19 @@ def main():
         quantity_input = quantity_input.strip()
 
         if not quantity_input.isdigit():
-                    print("Error! '" + quantity_input + "' is not a valid quantity. Please try again!")
-                    continue
+            print("Error! '" + quantity_input + "' is not a valid quantity. Please try again!")
+            continue
+
+        quantity = int(quantity_input)
+        new_id = get_order_id(orders)
+
+        orders.append([new_id, user_input, quantity])
+
+        print("\nNew order Added:")
+        print(str(new_id) + ", " + user_input + ", " + str(quantity))
+
+    save_inventory(orders)
+    print("\nOrder successfully saved to orders.txt")
+
 
 main()
